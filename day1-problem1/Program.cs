a8bf8b7795ce712de64d14bfdc9c6edae6b87f3c
FireStormOOO <josh@firestorm.space>
2024-12-01 20:20:58 -0700

    Complete day 1 problem 1

---
+++ day1-problem1/Program.cs
@@ -0,0 +1,16 @@
+﻿var a = new List<int>();
+var b = new List<int>();
+await using var stream = new FileStream("input.txt", FileMode.Open);
+using var reader = new StreamReader(stream);
+while (!reader.EndOfStream)
+{
+    var line = reader.ReadLine()!.Split("   ");
+    a.Add(int.Parse(line[0]));
+    b.Add(int.Parse(line[1]));
+}
+a.Sort();
+b.Sort();
+var res = a.Zip(b)
+    .Select(pair => Math.Abs(pair.First - pair.Second))
+    .Aggregate((l, r) => l + r);
+Console.WriteLine(res);
