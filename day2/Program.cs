abd31275890b0e351639586b713d20e033b7fe3a
FireStormOOO <josh@firestorm.space>
2024-12-02 00:17:06 -0700

    day 2 part 2

--- day2/Program.cs
+++ day2/Program.cs
@@ -4,21 +4,43 @@ var lines = new List<List<int>>();
 while (!reader.EndOfStream) 
     lines.Add(reader.ReadLine()!.Split(" ").Select(int.Parse).ToList());
 int safeCount = 0;
-foreach (var line in lines)
+
+bool CheckLineSafe(List<int> line, int deltaHigh, int deltaLow, int problemDampener = 1)
 {
     bool safe = true;
-    int deltaLow = -3, deltaHigh = -1;
-    if (line[0] < line[1])
-    {
-        deltaHigh = 3;
-        deltaLow = 1;
-    }
     for (int i = 1; i < line.Count && safe; i++)
     {
         int delta = line[i] - line[i - 1];
         safe = delta >= deltaLow && delta <= deltaHigh;
+        if (safe || problemDampener <= 0) continue;
+        
+        //handle problem dampener logic
+        problemDampener--;
+        if (i + 1 == line.Count)//we're already at the end and can drop the failing value
+        {
+            safe = true;
+            continue;
+        } 
+          
+        int delta2 = line[i + 1] - line[i - 1];
+        if (delta2 >= deltaLow && delta2 <= deltaHigh)//can we skip i?
+        {
+            safe = true;
+            i++;//if we skip i, jump forward twice
+            continue;
+        }
+
+        //if we're at the beginning, check if we can drop the first value
+        int delta3 = line[i + 1] - line[i];
+        safe = i==1 && delta3 >= deltaLow && delta3 <= deltaHigh;
     }
 
-    safeCount += safe ? 1 : 0;
+    return safe;
+}
+
+foreach (var line in lines)
+{
+    safeCount += CheckLineSafe(line, -1, -3) ? 1 : 0;
+    safeCount += CheckLineSafe(line, 3, 1) ? 1 : 0;
 }
 Console.WriteLine($"Safe line count: {safeCount}");
