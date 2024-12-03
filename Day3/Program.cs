7613b2262d21ba28e1462e938134aeb672979c30
FireStormOOO <josh@firestorm.space>
2024-12-02 22:28:29 -0700

    Day 3 part 1

---
+++ Day3/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Text.RegularExpressions;
+using static Day3.PuzzleInput;
+
+var matches = Regex.Matches(Input, @"mul\((\d{1,3}),(\d{1,3})\)");
+var res = matches.Select(match => int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value)).Sum();
+Console.WriteLine($"Sum of match evals: {res}");
