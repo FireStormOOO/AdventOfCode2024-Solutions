c0884fd0eecba3e496ef05997e5fc50e22d0b975
FireStormOOO <josh@firestorm.space>
2024-12-02 22:38:20 -0700

    Day 3 part 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,6 +1,15 @@
 ﻿using System.Text.RegularExpressions;
 using static Day3.PuzzleInput;
 
-var matches = Regex.Matches(Input, @"mul\((\d{1,3}),(\d{1,3})\)");
-var res = matches.Select(match => int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value)).Sum();
-Console.WriteLine($"Sum of match evals: {res}");
+var matches = Regex.Matches(Input, @"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)");
+bool enable = true;
+int tally = 0;
+foreach (Match match in matches)
+{
+    if (match.Value.StartsWith("mul(") && enable)
+        tally += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+    else if(match.Value.StartsWith("do(")) enable = true;
+    else if (match.Value.StartsWith("don't(")) enable = false;
+}
+
+Console.WriteLine($"Sum of match evals: {tally}");
