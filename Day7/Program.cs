350f21b82eee1e228ea4ff6cf092872e4da747c7
FireStormOOO <josh@firestorm.space>
2024-12-06 23:16:23 -0700

    Day 7 part 2

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -4,9 +4,12 @@ using Day7;
 
 long Add(long a, long b) => a + b;
 long Mul(long a, long b) => a * b;
-var ops = new[]{Add, Mul};
+long Concat(long a, long b) => (long)(a * Math.Pow(10, (long)Math.Log10(b) + 1L)) + b;
 long Calculate(string input, bool part1=true)
 {
+    var ops = new List<Func<long,long,long>>{Add, Mul};
+    if(!part1)
+        ops.Add(Concat);
     var matches = Regex.Matches(input, @"^(\d+): (\d+ ?){2,}$", RegexOptions.Multiline);
     var tally = 0L;
     foreach (Match match in matches)
@@ -16,9 +19,10 @@ long Calculate(string input, bool part1=true)
         var args = new List<long>();
         for(int i = 1; i < argMatch.Count; i++) args.Add(long.Parse(argMatch[i].Value));
         
-        for (int i = 0; i < 1 << (args.Count - 1); i++)
+        
+        for (int i = 0; i < Math.Pow(ops.Count, args.Count - 1); i++)
         {
-            Func<long, long, long> GetOp(int iOp) => ops[(i >> iOp) & 1];
+            Func<long, long, long> GetOp(int iOp) => ops[(int)(i / Math.Pow(ops.Count, iOp)) % ops.Count];
             long running = GetOp(0)(args[0], args[1]);
             for (int iOp = 1; iOp < args.Count -1; iOp++) 
                 running = GetOp(iOp)(running, args[iOp + 1]);
@@ -36,7 +40,7 @@ long Calculate(string input, bool part1=true)
 Debug.Assert(3749 == Calculate("190: 10 19\n3267: 81 40 27\n83: 17 5\n156: 15 6\n7290: 6 8 6 15\n161011: 16 10 13\n192: 17 8 14\n21037: 9 7 18 13\n292: 11 6 16 20"));
 Console.WriteLine($"Calibration sum is {Calculate(PuzzleInput.Input)}");
 
-//Debug.Assert(6 == Calculate("190: 10 19\n3267: 81 40 27\n83: 17 5\n156: 15 6\n7290: 6 8 6 15\n161011: 16 10 13\n192: 17 8 14\n21037: 9 7 18 13\n292: 11 6 16 20",false));
-//Console.WriteLine($"Guard can be looped in {Calculate(PuzzleInput.Input,false)} distinct places");
+Debug.Assert(11387 == Calculate("190: 10 19\n3267: 81 40 27\n83: 17 5\n156: 15 6\n7290: 6 8 6 15\n161011: 16 10 13\n192: 17 8 14\n21037: 9 7 18 13\n292: 11 6 16 20",false));
+Console.WriteLine($"Corrected calibration sum is  {Calculate(PuzzleInput.Input,false)}");
 
 Console.WriteLine($"Done!");
