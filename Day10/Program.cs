f8316ccfc24c0afb58a481cca97ec3196816a642
FireStormOOO <josh@firestorm.space>
2024-12-09 23:44:08 -0700

    Day 10 part 2

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -28,7 +28,7 @@ int Calculate(string input, bool part1=true)
         while (searchHeads.Count>0)
         {
             var adj = GetAdjacent(searchHeads[0])
-                .Where(xy=>!path.Union(goals).Union(searchHeads).Contains(xy)).ToList();
+                .Where(xy=>!part1 || !path.Union(goals).Union(searchHeads).Contains(xy)).ToList();
             var searchHeads0Val = grid.Index(searchHeads[0]);
             foreach (var tile in adj)
                 if (grid.Index(tile) == searchHeads0Val + 1)
@@ -39,7 +39,7 @@ int Calculate(string input, bool part1=true)
             searchHeads.RemoveAt(0);
         }
 
-        tally += goals.Distinct().Count();
+        tally += goals.Count();
     }
 
     return tally;
@@ -49,7 +49,9 @@ string testInput = "89010123\n78121874\n87430965\n96549874\n45678903\n32019012\n
 Debug.Assert(36 == Calculate(testInput));
 Console.WriteLine($"Sum of trailhead scores is {Calculate(PuzzleInput.Input)}");
 
-//Debug.Assert( == Calculate(testInput,false));
-//Console.WriteLine($"{Calculate(PuzzleInput.Input,false)}");
+Debug.Assert(81 == Calculate(testInput,false));
+string testInput2 = "012345\n123456\n234567\n345678\n416789\n567891";
+Debug.Assert(227 == Calculate(testInput2,false));
+Console.WriteLine($"Sum of corrected trailhead scores is {Calculate(PuzzleInput.Input,false)}");
 
 Console.WriteLine($"Done!");
