fbc4ad7f429a95e4c89a5a51213b726dea47ec90
FireStormOOO <josh@firestorm.space>
2024-12-10 22:28:38 -0700

    Day 11 part 1

---
+++ Day11/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Common;
+using Day11;
+
+long Calculate(string input, int blinks = 25)
+{
+    var stones = new NumArgsByLine<long>(input).Lines[0];
+
+    for (int blink = 0; blink < blinks; blink++)
+    {
+        var toSplit = new List<long>();
+        for (int iStone = 0; iStone < stones.Count; iStone++)
+        {
+            if (stones[iStone] == 0)
+                stones[iStone] = 1;
+            else if ((int)Math.Log10(stones[iStone]) % 2 == 1)
+            {
+                var str = stones[iStone].ToString();
+                var half = str.Length >> 1;
+                var left = long.Parse(str.Substring(0, half));
+                var right = long.Parse(str.Substring(half, half));
+                stones[iStone] = left;
+                stones.Insert(++iStone,right);
+            }
+            else
+                stones[iStone] *= 2024;
+        }
+
+        if (stones.Count > int.MaxValue >> 2)
+            throw new ArgumentOutOfRangeException();
+    }
+
+    return stones.Count;
+
+}
+
+string testInput = "125 17";
+Debug.Assert(55312 == Calculate(testInput));
+Console.WriteLine($"There are {Calculate(PuzzleInput.Input)} stones after 25 blinks");
+
+//Console.WriteLine($"There are {Calculate(PuzzleInput.Input,75)} stones after 75 blinks");
+
+Console.WriteLine($"Done!");
---
+++ Day11/PuzzleInput.cs
@@ -0,0 +1,6 @@
+namespace Day11;
+
+public static class PuzzleInput
+{
+    public const string Input = "5178527 8525 22 376299 3 69312 0 275";
+}
